44634a4d50e19921a885116b832ff320f5cb33c1
a-angeliev <nachko01@gmail.com>
2022-04-08 15:03:25 +0300

    Fixed Procfiel

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn project.wsgi
+web: gunicorn --pythonpath project.wsgi
--- project/settings.py
+++ project/settings.py
@@ -138,9 +138,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (BASE_DIR/'static/',)
+# STATIC_URL = 'static/'
+# #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (BASE_DIR/'static/',)
+
+
+BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR1, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
