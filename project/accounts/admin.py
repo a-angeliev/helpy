11c3bd079f74e86f816ed18a583b04c9f9fe0551
a-angeliev <nachko01@gmail.com>
2022-04-15 22:32:08 +0300

    Fixing admin panel view

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 
 from project.accounts.models import Profile
-from project.main.models import Shelter, Job, Campaign
 
 UserModel = get_user_model()
 
@@ -17,8 +16,6 @@ class SomeInline(admin.TabularInline):
 @admin.register(UserModel)
 class UserAdmin(admin.ModelAdmin):
     inlines = [SomeInline]
-    # inlines = (PetInlineAdmin,)
-    # list_display = ('first_name', 'last_name')
 
 
 @admin.register(Profile)
@@ -26,26 +23,6 @@ class ProfileAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Campaign)
-class ProfileAdmin(admin.ModelAdmin):
-    pass
 
 
-# class AllUserAdmin(admin.ModelAdmin):
-#     inlines = [UserAdmin, ProfileAdmin]
 
-
-@admin.register(Shelter)
-class ShelterAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    pass
-
-
-# @admin.register()
-# class ShelterAdmin(admin.ModelAdmin):
-#     # inlines = (PetInlineAdmin,)
-#     list_display = ('first_name', 'last_name')
--- project/main/admin.py
+++ project/main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from project.main.models import Questionnaire, Campaign, Shelter, Job
+
+
+@admin.register(Questionnaire)
+class Questionnaire(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(Shelter)
+class ShelterAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("title",)
